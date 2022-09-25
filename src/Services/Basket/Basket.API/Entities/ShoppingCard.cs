84173b701f88cf2cce51b47a74fa1e922bc8154c
muratsalar <murattsalar@gmail.com>
2022-09-25 21:53:27 +0300

    Redis connection added to Basket API

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCard
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCardItem> Items { get; set; } = new List<ShoppingCardItem>();
+
+        public ShoppingCard()
+        {
+
+        }
+        public ShoppingCard(string userName)
+        {
+            this.UserName = userName;  
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach(ShoppingCardItem item in Items)
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+                return totalPrice;
+            }
+        }
+    }
+}
